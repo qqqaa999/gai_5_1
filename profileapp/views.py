ff4a83d1772cdc74f01d9258b766ca9744afad03
qqqaa999 <qqqaa999@naver.com>
2021-07-29 09:37:23 +0900

    2021-07-29

--- profileapp/views.py
+++ profileapp/views.py
@@ -1,14 +1,19 @@
 from bs4.diagnose import profile
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse_lazy, reverse
+from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, UpdateView
 
+from profileapp.decorators import profile_ownership_required
 from profileapp.forms import ProfileCreationForm
 from profileapp.models import Profile
 
 
+@method_decorator(login_required, 'get')
+@method_decorator(login_required, 'post')
 class ProfileCreateView(CreateView):
     model = profile
     form_class = ProfileCreationForm
@@ -21,7 +26,8 @@ class ProfileCreateView(CreateView):
     def get_success_url(self):
         return  reverse('accountapp:detall',kwargs={'pk':self.object.pk})
 
-
+@method_decorator(profile_ownership_required, 'get')
+@method_decorator(profile_ownership_required, 'post')
 class ProfileUpdateView(UpdateView):
     model = Profile
     context_object_name = 'target_profile'
